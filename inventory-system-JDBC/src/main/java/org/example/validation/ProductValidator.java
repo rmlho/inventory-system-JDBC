b2cea0f6c00bed0c7c3d2393b1befcf2d6d43f2d
João Gabriel <joaogabriel.ejn@gmail.com>
2026-09-09 23:55:35 -0300

    refactor: removes throws from methods

--- inventory-system-JDBC/src/main/java/org/example/validation/ProductValidator.java
+++ inventory-system-JDBC/src/main/java/org/example/validation/ProductValidator.java
@@ -1,6 +1,9 @@
 package org.example.validation;
 
 import org.example.exception.ProductInvalidException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public final class ProductValidator {
 
@@ -8,35 +11,45 @@ public final class ProductValidator {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated.");
     }
 
-    public static void validateEmptyName(String name) throws ProductInvalidException {
+    public static void validateEmptyName(String name) {
         if (name == null || name.isBlank()) {
             throw new ProductInvalidException("Empty field.");
         }
     }
 
-    public static void validateNegative(int quatity) throws ProductInvalidException {
+    public static void validateNegative(int quatity) {
         if (quatity < 0) {
             throw new ProductInvalidException("A field cannot be negative.");
         }
     }
 
-    public static void validateValue(float value) throws ProductInvalidException {
+    public static void validateValue(float value) {
         if (value <= 0) {
             throw new ProductInvalidException("The value must be greater than zero.");
         }
     }
 
-    public static void confirmDeletion(int quantity) throws ProductInvalidException {
+    public static void confirmDeletion(int quantity) {
         if (quantity != 0) {
             throw new ProductInvalidException("A product cannot have an available quantity.");
         }
     }
 
-    public static void validatorSufficientInventory(int quantity, int availableQuantity) throws ProductInvalidException {
+    public static void validatorSufficientInventory(int quantity, int availableQuantity) {
         if (quantity > availableQuantity) {
             throw new ProductInvalidException("Do not have that quantity in stock.");
         }
     }
 
+    public static void validatorStatusActive(String productStatus) {
+        if (!Objects.equals(productStatus, "ATIVO")) {
+            throw new ProductInvalidException("This product is not active.");
+        }
+    }
 
+    public static void validatorString(@NotNull String text) {
+        if (!text.matches("^[A-Za-zÀ-ÿ ]+$")) {
+            throw new ProductInvalidException("Not is text.");
+        }
+    }
 }
