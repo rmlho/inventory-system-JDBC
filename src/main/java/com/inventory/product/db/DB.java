c2a48977bea78ba0b9b73e2d2992a69b3f460002
João Gabriel <joaogabriel.ejn@gmail.com>
2026-09-11 04:58:33 -0300

    feat: adds the connection to the database

--- src/main/java/com/inventory/product/db/DB.java
+++ src/main/java/com/inventory/product/db/DB.java
@@ -1,4 +1,19 @@
 package com.inventory.product.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DB {
+    private static String url = "jdbc:postgresql://localhost:5432/dbproduct";
+    private static String username = "postgres";
+    private static String password = "Lov31070601";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(username, username, username);
+        } catch (SQLException e) {
+            throw new RuntimeException("ERRO: " + e.getMessage(), e);
+        }
+    }
 }
