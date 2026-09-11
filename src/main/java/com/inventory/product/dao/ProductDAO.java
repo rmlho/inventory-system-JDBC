c6ebb41081293f62851ea14b4ecbb240df2c6c8a
João Gabriel <joaogabriel.ejn@gmail.com>
2026-09-11 04:59:00 -0300

    feat: Add the DAO interface

--- src/main/java/com/inventory/product/dao/ProductDAO.java
+++ src/main/java/com/inventory/product/dao/ProductDAO.java
@@ -1,4 +1,17 @@
 package com.inventory.product.dao;
 
-public class ProductDAO {
+import com.inventory.product.model.CategoryProductEnum;
+import com.inventory.product.model.Product;
+
+import java.util.List;
+
+public interface ProductDAO {
+    void insert(Product product);
+    List<Product> generalReport();
+    void update(int id, Product product);
+    void remove(int id);
+    List<Product> searchName(String name);
+    List<Product> higherValue();
+    List<Product> pricePerProduct();
+    List<Product> searchCategory(CategoryProductEnum category);
 }
